83d9192463ea0dd1ef3795d31515ed100bcbb8fb
fitandfine <emailofanup@gmail.com>
2026-01-05 19:21:58 +1300

    Added path parameter to support GET /convert/55 along with query parameter GET /convert?number=55

--- ntow/main.py
+++ ntow/main.py
@@ -8,6 +8,7 @@ def health_check():
     # Just a simple route to see if the server is alive
     return {"status": "online", "message": "Number to Words API is running"}
 
+# route to accept number as query parameter and return its word representation
 @app.get("/convert")
 def convert(number: int = Query(..., description="The number you want to turn into words")):
     # Basic validation: let's keep it to positive numbers for now
@@ -24,6 +25,25 @@ def convert(number: int = Query(..., description="The number you want to turn in
         "input": number,
         "output": words
     }
+
+@app.get("/convert/{number}")
+def convert(number: int):
+    # Basic validation: let's keep it to positive numbers for now
+    if number < 0:
+        raise HTTPException(status_code=400, detail="Please provide a positive integer.")
+    
+    if number > 999_999_999_999_999:
+        raise HTTPException(status_code=400, detail="Number too large. Please provide a number up to 999,999,999,999,999.")
+    
+    # Using our logic from logic.py
+    words = number_to_words(number)
+    
+    return {
+        "input": number,
+        "output": words
+    }
+
+
 @app.get("/about")
 def about():
     return {
